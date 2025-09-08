4b3e35978ba77b61e324e808c96f196fc863a821
Neelkanth Mundkar <youremail@example.com>
2025-09-09 00:57:04 +0530

    final push

---
+++ Routes/books.js
@@ -0,0 +1,181 @@
+const express=require('express');
+const {books} = require('../Data/books.json');
+const {users}= require('../Data/users.json');
+
+const router=express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Home Page",
+    data: books
+  });
+});
+
+router.post('/', (req, res) => {
+  const { id, title, author, price, publisher, genere} = req.body;
+
+  
+  const book = books.find((each) => each.id === Number(id));
+  if (book) {
+    return res.status(400).json({   
+      success: false,
+      message: "Book already exists"
+    });
+  }
+
+  
+  if (!id || !title || !author || !price || !publisher || !genere === undefined) {   
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required"
+    });
+  }
+
+  
+  books.push({ id, title, author, price, publisher, genere });
+
+  res.status(201).json({
+    success: true,
+    message: "Books created successfully"
+  });
+});
+
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const {body} = req.body;
+
+//   const book= books.find((each) => each.id === Number(id));
+//   if (!book) {
+//     return res.status(404).json({
+//       success: false,
+//       message: "book not found"
+//     });
+//   }
+
+//   const updatedbook= books.map((each) => {
+//     if (each.id === Number(id)) {
+//       return { ...each, ...body };
+//     }
+
+//     return each;
+// })
+//   res.status(200).json({
+//     success: true,
+//     data: updatedbook,
+//     message: "book updated successfully"
+    
+//   });
+// })
+
+//Updating the book by its id
+router.put('/books/:id', (req, res) => {
+
+const {id, title, author, price, publisher, genere}=req.body
+
+const book= books.find((each) => each.id === Number(id));
+if (!id || !title || !author || !price || publisher || genere === undefined) {
+  return res.status(404).json({
+    success: false,
+    message: "book not found"
+  });
+}
+const updatedbook= books.map((each) => {
+  if (each.id === Number(id)) {
+    return { ...each, ...body };
+  }
+    return each;
+})
+res.status(200).json({
+    success: true,
+    data: updatedbook,
+    message: "book updated successfully"
+})
+})
+
+router.put('/issuedbooks/:id', (req, res)=> {
+  const {id, available}=req.body
+  const book= books.find((each) => each.id === Number(id));
+  
+  if (!id || available === undefined) {
+    return res.status(404).json({
+      success: false,
+      message: "book not found"
+    });
+  }
+  const updatedbook= books.map((each) => {
+    if (each.id === Number(id)) {
+      return { ...each, available: false };
+    }
+      return each;
+  })
+  res.status(200).json({
+      success: true,
+      data: updatedbook,
+      message: "book issued successfully"
+  })
+})
+
+
+
+
+
+    router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+
+  const book = books.find((each) => each.id === Number(id));
+
+  if (!book) {
+    return res.status(404).json({
+      success: false,
+      message: "book not found"
+    })};
+  
+
+// const updatedbooks = books.filter((each) => each.id !== Number(id));
+
+  const index = books.indexOf(book);
+  books.splice(index, 1);
+  const updatedbooks = books;
+
+  res.status(200).json({
+    success: true,
+    data: updatedbooks,
+    message: "book deleted successfully"
+  });
+
+})
+
+router.get('/issued/for-users', (req, res) => {
+  // const issuedBooks = books.filter((each) => each.available === false);
+  const userWithIssuedBooks = users.filter((each) =>{
+    if(each.issuedBook){
+      return each;
+    }
+    
+  })
+
+  const issuedBooks = [];
+  userWithIssuedBooks.forEach((each) => {
+    const book = books.find((book) => book.id === Number(each.issuedBook));
+    book.issuedBy = each.name;
+    book.issuedDate = each.issuedDate;
+    book.returnDate = each.returnDate;
+    issuedBooks.push(book);
+  })
+
+  if(!issuedBooks){
+    return res.status(404).json({
+      success: false,
+      message: "No books are issued"
+  })}
+
+  res.status(200).json({
+    success: true,
+    data: issuedBooks,
+    message: "Issued books retrieved successfully"
+  });
+});
+
+
+module.exports=router;
---
+++ Routes/users.js
@@ -0,0 +1,124 @@
+const express=require('express');
+const {users} = require('../Data/users.json');
+
+const router=express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Home Page",
+    data: users
+  });
+});
+
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users
+  });
+});
+
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === Number(id));
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found"
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+
+router.post('/', (req, res) => {
+  const { id, name, email, role, isActive } = req.body;
+
+  
+  const user = users.find((each) => each.id === Number(id));
+  if (user) {
+    return res.status(400).json({   
+      success: false,
+      message: "User already exists"
+    });
+  }
+
+  
+  if (!id || !name || !email || !role || isActive === undefined) {   
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required"
+    });
+  }
+
+  
+  users.push({ id, name, email, role, isActive });
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully"
+  });
+});
+
+router.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const {body} = req.body;
+
+  const user= users.find((each) => each.id === Number(id));
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found"
+    });
+  }
+
+  const updatedUser= users.map((each) => {
+    if (each.id === Number(id)) {
+      return { ...each, ...body };
+    }
+
+    return each;
+})
+  res.status(200).json({
+    success: true,
+    data: updatedUser,
+    message: "User updated successfully"
+    
+  });
+})
+
+
+router.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+
+  const user = users.find((each) => each.id === Number(id));
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found"
+    })};
+  
+
+// const updatedUsers = users.filter((each) => each.id !== Number(id));
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+  const updatedUsers = users;
+
+  res.status(200).json({
+    success: true,
+    data: updatedUsers,
+    message: "User deleted successfully"
+  });
+
+})
+
+module.exports=router;
